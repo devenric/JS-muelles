781b53804b37533718fedd6f99641851e468735c
enricdev <engato@alumnatflorida.es>
2025-03-12 14:25:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+for (let i=1; i<4; i++){
+setTimeout(function(){
+const resultado = document.createElement('p');
+    if (i===1){
+    resultado.textContent = `cojo ${i} muelle, lo tiro por el retrete, ya es ${i} muelle que el retrete se ha tragado`;
+}
+            else { resultado.textContent = `cojo ${i} muelles, los tiro por el retrete, ya son ${i} muelles que el retrete se ha tragado`;
+    }
+document.getElementById('resultados').appendChild(resultado);
+},i * 3500);
 
+}
