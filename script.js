da1b35d972d9ad519f2d63fa3a750d3ffecf1c9d
enricdev <engato@alumnatflorida.es>
2025-03-17 09:50:54 +0100

    comprension muelles v1

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+let resultados = document.getElementById('resultados')
 for (let i=1; i<10; i++){
-setTimeout(function(){
+    setTimeout(function(){
 const resultado = document.createElement('p');
     if (i===1){
     resultado.textContent = `cojo ${i} muelle, lo tiro por el retrete, ya es ${i} muelle que el retrete se ha tragado`;
 }
             else { resultado.textContent = `cojo ${i} muelles, los tiro por el retrete, ya son ${i} muelles que el retrete se ha tragado`;
     }
-document.getElementById('resultados').appendChild(resultado);
+document.appendChild(resultado);
 },i * 2000);
 
 }
